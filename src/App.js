26428183a47b669686af46b188878ce2638e2ad0
Giulio Baia <51792545+razak57@users.noreply.github.com>
2021-11-13 23:48:10 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,16 @@
 import React from "react";
-import styled from "styled-components";
-import { Cards, CountryPicker, Chart } from "./components";
-import { fetchData } from "./api/";
-import image from "./images/image.png";
-import { useState, useEffect } from "react";
 import { CovidProvider } from "./context/covidContext";
+import CovidTracker from "./context/covidTracker";
 
-const StyledContainer = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-`;
-
-const StyledImage = styled.image`
-  width: 370px;
-  margin-top: 50px;
-`;
 
 const App = () => {
-  const [data, setData] = useState({});
-  const [country, setCountry] = useState(null);
-
-  useEffect(() => {
-    const fetcher = async () => {
-      const data = await fetchData(country);
-      setData(data);
-    };
-    fetcher();
-  }, [country]);
-
-  const handleCountryChange = (e) => {
-    setCountry(e.value);
-  };
-
-  render() {
-  const { data, country } = this.state;
-
-    return (
-      <>
-        <CovidProvider>
-          <StyledContainer>
-            <StyledImage src={image} alt={"COVID-19"} />
-            <Cards data={data} />
-            <CountryPicker handleCountryChange={handleCountryChange} />
-            <Chart data={data} country={country} />
-          </StyledContainer>
-        </CovidProvider>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <CovidProvider>
+        <CovidTracker />
+      </CovidProvider>
+    </>
+  );
+};
 
 export default App;
